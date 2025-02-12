7222aabad52c1b5fdff6ebf1ffd885a57719d04d
Bernadette Modi <berniemain353@gmail.com>
2025-02-12 15:33:31 +0300

    Defined the function that asks the users how they like their egg.

--- eggcookingtimer.py
+++ eggcookingtimer.py
@@ -1,1 +1,10 @@
 import time
+
+def egg_timer():
+    print("How do you like you egg?")
+    print("1. Soft-Bioled")
+    print("2. Medium-Boiled")
+    print("3. Hard-Boiled")
+
+    choice = input("Enter the number of your choice: ")
+    
