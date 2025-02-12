69167c1fd42501d4fce7d1653fb30782f65bf803
Bernadette Modi <berniemain353@gmail.com>
2025-02-12 15:38:42 +0300

    Show cased what cooking time will be needed after the user choice.

--- eggcookingtimer.py
+++ eggcookingtimer.py
@@ -7,4 +7,14 @@ def egg_timer():
     print("3. Hard-Boiled")
 
     choice = input("Enter the number of your choice: ")
-    
+
+    if choice == '1':
+        cook_time = 5
+    elif choice == '2':
+        cook_time = 8
+    elif choice == '3':
+        cook_time = 12
+    else:
+        print("Invalid choice! Please select a valid option.")
+        return 
+        
